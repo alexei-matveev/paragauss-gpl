5d3a7667eb7b763bab2ac917aefe564f6aad2858
Alexei Matveev <matveev@theochem.tu-muenchen.de>
2014-06-13 16:04:38 +0200

    AM: memoization of n-ary functions, from BGY3D

--- guile/utils.scm
+++ guile/utils.scm
@@ -58,16 +58,16 @@
 
 
 ;;;
-;;; Let-over-lambda  here.   Given a  function  (f  x)  The result  of
-;;; (memoize f)  is a  function (f' x)  == (f  x) that will  cache all
-;;; results.  FIXME: make it work for arbitrary number of arguments.
+;;; Let-over-lambda here.  Given  a function (f x ...)   The result of
+;;; (memoize f) is a function (f' x ...)  == (f x ...) that will cache
+;;; all results.
 ;;;
 (define (memoize f)
   (let ((*cache* '()))                  ; empty cache
-    (lambda (x)
-      (cdr (or (assoc x *cache*)        ; first check the cache
-               (let* ((y (f x))         ; otherwise invoke f
-                      (p (cons x y)))   ; make dictionary pair
+    (lambda xs
+      (cdr (or (assoc xs *cache*)       ; first check the cache
+               (let* ((y (apply f xs))  ; otherwise invoke f
+                      (p (cons xs y)))  ; make dictionary pair
                  (set! *cache* (cons p *cache*)) ; cache new pair
                  p))))))                         ; and return it too
 

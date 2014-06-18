1fdb29967d40850a0b35ce85cad5a3610f2eec5e
Alexei Matveev <alexei.matveev@gmail.com>
2014-06-18 22:17:16 +0200

    AM: prec.pl in one line

--- qa/bin/prec.pl
+++ qa/bin/prec.pl
@@ -10,8 +10,9 @@ $DD =$D+1;
 #warn("## all numbers will be cut off to $D digits\n");
 
 while(<>){
-	while(/\d+\.\d{$DD,}/o){
-		s/(\d+)\.(\d{$D})(\d+)/\1.\2/;
-	}
+#	while(/\d+\.\d{$DD,}/o){
+#		s/(\d+)\.(\d{$D})(\d+)/\1.\2/;
+#	}
+	s/(\d+\.\d{$D})[0-9]*/\1/og;
 	print;
 }

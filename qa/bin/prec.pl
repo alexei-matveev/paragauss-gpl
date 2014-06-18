a85d3d56be226ef7b527c1ebfd4c62d1ae4b1fb9
Alexei Matveev <alexei.matveev@gmail.com>
2014-06-18 22:32:12 +0200

    AM: another (unsuccesfull?) attempt to round off

--- qa/bin/prec.pl
+++ qa/bin/prec.pl
@@ -6,12 +6,14 @@ if($ARGV[0] =~ /-(\d+)/){
 	shift(@ARGV);
 }
 $DD =$D+1;
+$pow = 10**$D;
 
 #warn("## all numbers will be cut off to $D digits\n");
 
 while(<>){
-#	while(/\d+\.\d{$DD,}/o){
-#		s/(\d+)\.(\d{$D})(\d+)/\1.\2/;
+#	while(/(\d+\.\d{$DD,})/o){
+#		$r = int( $1 * $pow + 0.5 )/$pow;
+#		s/\d+\.\d{$DD,}/$r/;
 #	}
 	s/(\d+\.\d{$D})[0-9]*/\1/og;
 	print;

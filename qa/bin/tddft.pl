98338a9eda2f3de4604376cf2fab23e093d0e23d
Sergey Bosko <bosko@theochem.tu-muenchen.de>
2014-06-18 22:28:51 +0200

    SB: perl script

---
+++ qa/bin/tddft.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+#
+# $Id: geom.pl,v 1.1 2001/07/11 13:30:29 matveev Exp matveev $
+# $Log: geom.pl,v $
+# Revision 1.1  2001/07/11  13:30:29  matveev
+# Initial revision
+#
+#
+# Extracts geometry part from output
+#
+
+$txt = "";
+while( <> ){
+	if( /EXCITATIONS/../  =====END=====/ ){
+          print;
+	}
+}
+
+

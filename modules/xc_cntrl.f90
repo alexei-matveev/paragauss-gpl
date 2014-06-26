alexei-matveev/paragauss-gpl

commit 224dd9a18aa3245f88a126d4d60f39a00c047105

AM: xc_nl_calc_ph replaces nl_calc_ph from ph_cntrl module

modules/xc_cntrl.f90
@@ -152,7 +152,12 @@ module xc_cntrl
 
   integer(IK), private :: iyes = 1
 
-  logical, public   :: xc_nl_calc ! == is_on(xc_GGA), DEPRECATED
+  ! xc_nl_calc == is_on (xc_GGA), DEPRECATED
+  logical, public, protected :: xc_nl_calc
+
+  ! This one is true in  "energy" runs to allow computing all possible
+  ! GGAs. In gradient runs it is the same as xc_nl_calc.
+  logical, public, protected :: xc_nl_calc_ph
 
   real(RK), public  :: xc_sdens_cutoff = 1.0E-20_rk
   real(RK), private :: df_sdens_cutoff = 1.0E-20_rk
@@ -384,6 +389,7 @@ contains
 
   subroutine set_global()
     ! compatibility
+    use operations_module, only: operations_gradients
     implicit none
     ! *** end of interface ***
 
@@ -416,7 +422,18 @@ contains
 
     call set_functional_type()
 
-    xc_nl_calc = is_on(xc_GGA)
+    xc_nl_calc = is_on (xc_GGA)
+
+    ! By default we do all  GGAs in single-point post-scf run, but see
+    ! next:
+    xc_nl_calc_ph = .true.
+
+    ! Energy should be consistent with forces, in this case. Gradients
+    ! of XC term can only be computed for one functional.
+    if (operations_gradients) then
+        xc_nl_calc_ph = is_on (xc_gga)
+    endif
+
     DPRINT 'xccntl::set_global: Options=',Options
     DPRINT 'xccntl::set_global: xc_nl_calc=',xc_nl_calc
     DPRINT 'xccntl::set_global: exit'
@@ -1496,7 +1513,6 @@ contains
 
   end subroutine xc_write_input
 
-  !*****************************************************************************
 
   subroutine xc_input_bcast()
     ! Purpose : broadcasting the xc_input
@@ -1509,6 +1525,7 @@ contains
     call comm_bcast( frac            )
     call comm_bcast( xc_sdens_cutoff )
 
+    ! This will also set the public flags xc_nl_calc, xc_nl_calc_ph:
     if ( comm_rank() /= 0 ) call set_global()
 
   end subroutine xc_input_bcast

modules/ph_cntrl.f90
@@ -66,16 +66,6 @@ module ph_cntrl
 
   !------------ Declaration of constants and variables ------------
 
-  !
-  ! This flag is used in post_scf_module to indicate a GGA run:
-  !
-  logical, public, protected :: nl_calc_ph
-  !
-  ! FIXME: This flag and compatibility reasons (accepting older inputs)
-  !        are the only reason for existence of this module.
-  !        Get rid of it, whoever has the courage.
-  !
-
   !------------ Interface statements ------------------------------
 
   !------------ public functions and subroutines ------------------
@@ -186,31 +176,15 @@ contains
   end subroutine post_scf_read_input
 
   subroutine post_scf_set_defaults()
-    use xc_cntrl, SCF_ON => IS_ON
-    use operations_module, only: operations_gradients
     implicit none
     !** End of interface **************************************
 
-    !
-    ! By default we do all GGAs in single-point post-scf run:
-    !
-    nl_calc_ph = .true.
-
-    !
-    ! Energy should be consistent with forces, in this case:
-    !
-    if ( operations_gradients ) then
-        nl_calc_ph = SCF_ON(xc_gga)
-    endif
   end subroutine post_scf_set_defaults
 
   subroutine post_scf_input_bcast()
-    use comm, only: comm_bcast
     implicit none
     ! Purpose : broadcasting the phxc_input
     !** End of interface *****************************************
-
-    call comm_bcast(nl_calc_ph)
 
   end subroutine post_scf_input_bcast
 

modules/post_scf_module.f90
@@ -325,8 +325,7 @@ contains
     use time_module
     use unique_atom_methods, only: unique_atom_grad_information
     use iounitadmin_module
-    use xc_cntrl, only: is_on, xc_mgga
-    use ph_cntrl, only: nl_calc_ph
+    use xc_cntrl, only: is_on, xc_mgga, nl_calc_ph => xc_nl_calc_ph
     use pointcharge_module
     use xc_func, only: xc_func_reset
     use integralpar_module, only: integralpar_2dervs,integralpar_cpksdervs
@@ -1422,7 +1421,7 @@ contains
     use time_module
     use unique_atom_methods, only: unique_atom_grad_information
     use iounitadmin_module
-    use ph_cntrl, only:  nl_calc_ph
+    use xc_cntrl, only:  nl_calc_ph => xc_nl_calc_ph
     use pointcharge_module
     use integralpar_module, only: integralpar_2dervs,integralpar_cpksdervs
     use cpksdervs_matrices,only:cpksalloc,cpks,cpks_Qxc_calculated
@@ -2085,7 +2084,6 @@ contains
 
   subroutine post_scf_write_results
     use iounitadmin_module
-    use ph_cntrl
     use xc_func, only: xc_func_write
     implicit none
     ! purpose : write the results of the post_scf part
@@ -2213,9 +2211,9 @@ contains
     !** End of interface *****************************************
     use time_module
     use timer_module
-    use ph_cntrl
     use iounitadmin_module
     use xc_func
+    use xc_cntrl, only: nl_calc_ph => xc_nl_calc_ph
     implicit none
 
     integer(i4_kind) :: vla, xyz
@@ -2585,7 +2583,6 @@ contains
     !** End of interface *****************************************
     use time_module
     use timer_module
-    use ph_cntrl
     use xc_func ! exc_*, xc_functionals
     use cpks_grid_utils, only: symWtsNGra, cartNGra, cartNDer
     use cpks_xc_resp   , only: xc_resp_lda
@@ -3335,7 +3332,6 @@ contains
     !** End of interface *****************************************
     use time_module
     use timer_module
-    use ph_cntrl
     use xc_func ! exc_*, xc_functionals
     implicit none
     integer(i4_kind) :: i,ii,i_ea,i_ua,i_spin,k,i_vec,i_ma,i_m
@@ -3513,8 +3509,8 @@ contains
     !           - calcularions of derivatives not implemented yet
     use time_module
     use timer_module
-    use ph_cntrl
-    use xc_cntrl, only: xc_hcth_version, is_on, xc_mgga
+    use xc_cntrl, only: xc_hcth_version, is_on, xc_mgga, &
+         nl_calc_ph => xc_nl_calc_ph
     use xc_func ! exc_*, xc_functionals
 
     use cpksdervs_matrices, only: cpks, n_cpks, i_cpks, cpks_Qxc_calculated, &
@@ -5209,7 +5205,6 @@ contains
     !** End of interface *****************************************
     use time_module
     use timer_module
-    use ph_cntrl
     use xc_cntrl, only: xc_hcth_version
     use xc_func ! exc_*, xc_functionals
     use integralpar_module, only: integralpar_2dervs
@@ -5520,9 +5515,9 @@ contains
     use timer_module
 !temp comment    use rspace_plot
     use gamma_module
-    use ph_cntrl
     use iounitadmin_module
     use xc_func ! exc_*, xc_functionals
+    use xc_cntrl, only: nl_calc_ph => xc_nl_calc_ph
     implicit none
     real(kind=r8_kind), allocatable :: grad_rho(:,:,:), sec_der_rho(:,:,:), &
                                        gamm(:,:), dfdgrarho(:,:), &

commit 362a45c594329c96e3201b32522a534067f199ca

AM: FIX GGA calculations with legacy inputs
This should fix the regression introduced in 224dd9a for the case when
an old input which does not make use of XC= keyword and instead
specifies VWNC=, XALPHA=, BECKE99=, and PERDEW= flags.

modules/xc_cntrl.f90
@@ -441,6 +441,7 @@ contains
 
   subroutine set_options()
     ! compatibility
+    use operations_module, only: operations_gradients
     implicit none
     ! *** end of interface ***
 
@@ -474,6 +475,16 @@ contains
     call set_functional_type()
 
     xc_nl_calc = is_on(xc_GGA)
+
+    ! By default we do all  GGAs in single-point post-scf run, but see
+    ! next:
+    xc_nl_calc_ph = .true.
+
+    ! Energy should be consistent with forces, in this case. Gradients
+    ! of XC term can only be computed for one functional.
+    if (operations_gradients) then
+        xc_nl_calc_ph = is_on (xc_gga)
+    endif
   end subroutine set_options
 
   subroutine xc_read_input()
@@ -1335,10 +1346,14 @@ contains
          call input_error("xc_read: ambiguous settings in keyword XC")
        endif
 
+       ! Sets xc_nl_calc, xc_nl_calc_ph too:
        call set_global()
     else
+       ! Sets xc_nl_calc, xc_nl_calc_ph too:
        call set_options()
     endif
+    ! FIXME: get rid of legacy per-contribution globals! Otherwise one
+    ! would always need to maintain two branches as above.
 
     ! Now determine the type of functional i.e. the step in jakobs ladder
     call set_functional_type()
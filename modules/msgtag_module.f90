caad5b8cc603dc86044a4dc7ebc461b4dd08a072
Alexei Matveev <alexei.matveev@gmail.com>
2014-07-02 16:53:59 +0200

    AM: comment possibly unused message tags
    It is possible that with different build flags some of the tags are
    actually used. But there is no simple way to trace those.

--- modules/msgtag_module.f90
+++ modules/msgtag_module.f90
@@ -57,37 +57,37 @@ save
 !     msgtag_pert   "Start perturbation theory, son!"
 !     msgtag_end    "Sabbath, my son!"
 #define INTEGER_T       integer(kind=i4_kind), parameter, public
-INTEGER_T :: msgtag_pert=                   2
+!NTEGER_T :: msgtag_pert=                   2
 INTEGER_T :: msgtag_finito=                 5
 INTEGER_T :: msgtag_eigensolve=             6
-INTEGER_T :: msgtag_borders=                8
+!NTEGER_T :: msgtag_borders=                8
 INTEGER_T :: msgtag_commdata=              11
-INTEGER_T :: msgtag_debugorbitals=         13
+!NTEGER_T :: msgtag_debugorbitals=         13
 INTEGER_T :: msgtag_SendBackEig=           15
-INTEGER_T :: msgtag_eigvec=                17
-INTEGER_T :: msgtag_xcham_send=            19
-INTEGER_T :: msgtag_SendBackHam=           22
+!NTEGER_T :: msgtag_eigvec=                17
+!NTEGER_T :: msgtag_xcham_send=            19
+!NTEGER_T :: msgtag_SendBackHam=           22
 INTEGER_T :: msgtag_xcfitham_send=         23
 INTEGER_T :: msgtag_slavetiming=           27
-INTEGER_T :: msgtag_int_shutdown=          31
+!NTEGER_T :: msgtag_int_shutdown=          31
 INTEGER_T :: msgtag_int_2cff_result=       37
 INTEGER_T :: msgtag_int_2cob3c_result=     44
-INTEGER_T :: msgtag_int_rel_setup=         46
-INTEGER_T :: msgtag_int_rel_shutdown=      47
+!NTEGER_T :: msgtag_int_rel_setup=         46
+!NTEGER_T :: msgtag_int_rel_shutdown=      47
 INTEGER_T :: msgtag_int_2cob3c_filesize=   48
-INTEGER_T :: msgtag_ph_send=               51
+!NTEGER_T :: msgtag_ph_send=               51
 INTEGER_T :: msgtag_grad_3c=               56
 INTEGER_T :: msgtag_grad_ch=               58
-INTEGER_T :: msgtag_occ_levels_eigval=     59
+!NTEGER_T :: msgtag_occ_levels_eigval=     59
 INTEGER_T :: msgtag_fit_coeff_send=        60
-INTEGER_T :: msgtag_rel_Ttrans =           72
-INTEGER_T :: msgtag_rel_Ttrans_back =      73
-INTEGER_T :: msgtag_rel_momspace =         74
-INTEGER_T :: msgtag_rel_momspace_back =    75
-INTEGER_T :: msgtag_rel_send_overlap=      76
-INTEGER_T :: msgtag_rel_trafomat=          77
-INTEGER_T :: msgtag_SendBackTrafomat=      78
-INTEGER_T :: msgtag_rel_gradient_send=     80
+!NTEGER_T :: msgtag_rel_Ttrans =           72
+!NTEGER_T :: msgtag_rel_Ttrans_back =      73
+!NTEGER_T :: msgtag_rel_momspace =         74
+!NTEGER_T :: msgtag_rel_momspace_back =    75
+!NTEGER_T :: msgtag_rel_send_overlap=      76
+!NTEGER_T :: msgtag_rel_trafomat=          77
+!NTEGER_T :: msgtag_SendBackTrafomat=      78
+!NTEGER_T :: msgtag_rel_gradient_send=     80
 INTEGER_T :: msgtag_xcmdaham_send=         81
 INTEGER_T :: msgtag_build_xcmda=           82
 INTEGER_T :: msgtag_vir_levels=            85
@@ -95,24 +95,24 @@ INTEGER_T :: msgtag_rot_levels=            87
 INTEGER_T :: msgtag_int_dipole_result=     93
 INTEGER_T :: msgtag_response_setup=        95
 INTEGER_T :: msgtag_response_level_send=   96
-INTEGER_T :: msgtag_response_chmat=        97
+!NTEGER_T :: msgtag_response_chmat=        97
 INTEGER_T :: msgtag_response_2index=       98
-INTEGER_T :: msgtag_resp_2in_sl_start=     99
+!NTEGER_T :: msgtag_resp_2in_sl_start=     99
 INTEGER_T :: msgtag_response_2in_send=    100
-INTEGER_T :: msgtag_response_2in_send2=   101
-INTEGER_T :: msgtag_response_3index=      102
-INTEGER_T :: msgtag_resp_sl_start_3ind=   103
-INTEGER_T :: msgtag_resp_3ind_fitdim=     104
-INTEGER_T :: msgtag_response_3ind_send=   105
+!NTEGER_T :: msgtag_response_2in_send2=   101
+!NTEGER_T :: msgtag_response_3index=      102
+!NTEGER_T :: msgtag_resp_sl_start_3ind=   103
+!NTEGER_T :: msgtag_resp_3ind_fitdim=     104
+!NTEGER_T :: msgtag_response_3ind_send=   105
 INTEGER_T :: msgtag_response_3Clb_start=  106
-INTEGER_T :: msgtag_resp_sl_start_3Clb=   107
+!NTEGER_T :: msgtag_resp_sl_start_3Clb=   107
 INTEGER_T :: msgtag_response_3Clb_send=   108
-INTEGER_T :: msgtag_send_all_levels=      111
-INTEGER_T :: msgtag_resp_3Clb_tmptape=    112
-INTEGER_T :: msgtag_resp_3Clb_tmptape_2=  113
-INTEGER_T :: msgtag_grid_send_n_points =  122
-INTEGER_T :: msgtag_grid_send_points=     123
-INTEGER_T :: msgtag_int_setup=            124
+!NTEGER_T :: msgtag_send_all_levels=      111
+!NTEGER_T :: msgtag_resp_3Clb_tmptape=    112
+!NTEGER_T :: msgtag_resp_3Clb_tmptape_2=  113
+!NTEGER_T :: msgtag_grid_send_n_points =  122
+!NTEGER_T :: msgtag_grid_send_points=     123
+!NTEGER_T :: msgtag_int_setup=            124
 INTEGER_T :: msgtag_space_point=          125
 INTEGER_T :: msgtag_finish_poten=         128
 INTEGER_T :: msgtag_geom_grad=            130
@@ -121,8 +121,8 @@ INTEGER_T :: msgtag_surf_point_sa=        135
 INTEGER_T :: msgtag_sp_grinfo_dealloc=    136
 INTEGER_T :: msgtag_start_field=          137
 INTEGER_T :: msgtag_finish_field=         138
-INTEGER_T :: msgtag_start_rho=            139
-INTEGER_T :: msgtag_finish_rho=           140
+!NTEGER_T :: msgtag_start_rho=            139
+!NTEGER_T :: msgtag_finish_rho=           140
 INTEGER_T :: msgtag_free_bnds_fld=        143
 INTEGER_T :: msgtag_epe_data_sent=        146
 INTEGER_T :: msgtag_epe_do_gradients=     147
@@ -142,51 +142,51 @@ INTEGER_T :: msgtag_epe_def_fin=          160
 INTEGER_T :: msgtag_stop_daemon    =      161
 INTEGER_T :: msgtag_packed_message =      163
 ! added by HH in pg_V2.1_hh (was 124-139, changed to ):
-INTEGER_T :: msgtag_response_rhs_h=       167
-INTEGER_T :: msgtag_resp_sl_start_rhs=    168
-INTEGER_T :: msgtag_resp_rhs_fitdim=      169
-INTEGER_T :: msgtag_response_rhs_send=    170
-INTEGER_T :: msgtag_resp_rho1dat=         171
-INTEGER_T :: msgtag_resp_rho1dat_1b=      172
-INTEGER_T :: msgtag_resp_all3index=       173
-INTEGER_T :: msgtag_resp_all3Clb_start=   174
-INTEGER_T :: msgtag_resp_addFcoul=        175
-INTEGER_T :: msgtag_response_FG=          176
-INTEGER_T :: msgtag_resp_FGstart=         177
-INTEGER_T :: msgtag_resp_FGslave=         178
-INTEGER_T :: msgtag_resp_FGmaster=        179
-INTEGER_T :: msgtag_resp_Fcoulstart=      180
-INTEGER_T :: msgtag_resp_Fcoulslave=      181
-INTEGER_T :: msgtag_resp_Fcoulmaster=     182
-INTEGER_T :: msgtag_cpks_xc           =   188
+!NTEGER_T :: msgtag_response_rhs_h=       167
+!NTEGER_T :: msgtag_resp_sl_start_rhs=    168
+!NTEGER_T :: msgtag_resp_rhs_fitdim=      169
+!NTEGER_T :: msgtag_response_rhs_send=    170
+!NTEGER_T :: msgtag_resp_rho1dat=         171
+!NTEGER_T :: msgtag_resp_rho1dat_1b=      172
+!NTEGER_T :: msgtag_resp_all3index=       173
+!NTEGER_T :: msgtag_resp_all3Clb_start=   174
+!NTEGER_T :: msgtag_resp_addFcoul=        175
+!NTEGER_T :: msgtag_response_FG=          176
+!NTEGER_T :: msgtag_resp_FGstart=         177
+!NTEGER_T :: msgtag_resp_FGslave=         178
+!NTEGER_T :: msgtag_resp_FGmaster=        179
+!NTEGER_T :: msgtag_resp_Fcoulstart=      180
+!NTEGER_T :: msgtag_resp_Fcoulslave=      181
+!NTEGER_T :: msgtag_resp_Fcoulmaster=     182
+!NTEGER_T :: msgtag_cpks_xc           =   188
 INTEGER_T :: msgtag_cpks_solv1        =   189
 INTEGER_T :: msgtag_cpks_solv2        =   190
 INTEGER_T :: msgtag_cpks_solv3        =   191
 INTEGER_T :: msgtag_cpks_solv4        =   192
 INTEGER_T :: msgtag_resp_2center=         189
-INTEGER_T :: msgtag_resp_3center_co=      190
+!NTEGER_T :: msgtag_resp_3center_co=      190
 INTEGER_T :: msgtag_tddft_eps_eta=        191
 INTEGER_T :: msgtag_tddft_clshell=        192
-INTEGER_T :: msgtag_tddft_opshell=        193
+!NTEGER_T :: msgtag_tddft_opshell=        193
 INTEGER_T :: msgtag_tddft_diagonl=        194
-INTEGER_T :: msgtag_tddft_diagnup=        195
-INTEGER_T :: msgtag_tddft_diagndn=        196
-INTEGER_T :: msgtag_tddft_parmlt2=        198
+!NTEGER_T :: msgtag_tddft_diagnup=        195
+!NTEGER_T :: msgtag_tddft_diagndn=        196
+!NTEGER_T :: msgtag_tddft_parmlt2=        198
 INTEGER_T :: msgtag_tddft_parmlt3=        199
-INTEGER_T :: msgtag_tddft_parmlt4=        200
+!NTEGER_T :: msgtag_tddft_parmlt4=        200
 INTEGER_T :: msgtag_tddft_dipolem=        201
-INTEGER_T :: msgtag_tddft_diagsnd=        202
+!NTEGER_T :: msgtag_tddft_diagsnd=        202
 INTEGER_T :: msgtag_tddft_sendeig=        203
 INTEGER_T :: msgtag_tddft_dealloM=        204
 INTEGER_T :: msgtag_send_2c_Colmb=        205
-INTEGER_T :: msgtag_send_3c_Colmb=        206
+!NTEGER_T :: msgtag_send_3c_Colmb=        206
 INTEGER_T :: msgtag_send_2c_start=        207
 INTEGER_T :: msgtag_tmp_3co_send =        208
 INTEGER_T :: msgtag_nori_2c_send =        209
 INTEGER_T :: msgtag_ind_dipmom   =        210
 INTEGER_T :: msgtag_Pol_ham      =        211
 INTEGER_T :: msgtag_back_Pol_ham =        212
-INTEGER_T :: msgtag_end=                  254
+!NTEGER_T :: msgtag_end=                  254
 INTEGER_T :: msgtag_error=                255
 !..............................................
 !message tags >= 1000 are reserved for molecular
@@ -208,37 +208,37 @@ contains
 
 #define CMP(x) if(tag==x) name=__STRING(x)
     name = 'undef'
-    CMP(msgtag_pert)
+    !MP(msgtag_pert)
     CMP(msgtag_finito)
     CMP(msgtag_eigensolve)
-    CMP(msgtag_borders)
+    !MP(msgtag_borders)
     CMP(msgtag_commdata)
-    CMP(msgtag_debugorbitals)
+    !MP(msgtag_debugorbitals)
     CMP(msgtag_SendBackEig)
-    CMP(msgtag_eigvec)
-    CMP(msgtag_xcham_send)
-    CMP(msgtag_SendBackHam)
+    !MP(msgtag_eigvec)
+    !MP(msgtag_xcham_send)
+    !MP(msgtag_SendBackHam)
     CMP(msgtag_xcfitham_send)
     CMP(msgtag_slavetiming)
-    CMP(msgtag_int_shutdown)
+    !MP(msgtag_int_shutdown)
     CMP(msgtag_int_2cff_result)
     CMP(msgtag_int_2cob3c_result)
-    CMP(msgtag_int_rel_setup)
-    CMP(msgtag_int_rel_shutdown)
+    !MP(msgtag_int_rel_setup)
+    !MP(msgtag_int_rel_shutdown)
     CMP(msgtag_int_2cob3c_filesize)
-    CMP(msgtag_ph_send)
+    !MP(msgtag_ph_send)
     CMP(msgtag_grad_3c)
     CMP(msgtag_grad_ch)
-    CMP(msgtag_occ_levels_eigval)
+    !MP(msgtag_occ_levels_eigval)
     CMP(msgtag_fit_coeff_send)
-    CMP(msgtag_rel_Ttrans)
-    CMP(msgtag_rel_Ttrans_back)
-    CMP(msgtag_rel_momspace)
-    CMP(msgtag_rel_momspace_back)
-    CMP(msgtag_rel_send_overlap)
-    CMP(msgtag_rel_trafomat)
-    CMP(msgtag_SendBackTrafomat)
-    CMP(msgtag_rel_gradient_send)
+    !MP(msgtag_rel_Ttrans)
+    !MP(msgtag_rel_Ttrans_back)
+    !MP(msgtag_rel_momspace)
+    !MP(msgtag_rel_momspace_back)
+    !MP(msgtag_rel_send_overlap)
+    !MP(msgtag_rel_trafomat)
+    !MP(msgtag_SendBackTrafomat)
+    !MP(msgtag_rel_gradient_send)
     CMP(msgtag_xcmdaham_send)
     CMP(msgtag_build_xcmda)
     CMP(msgtag_vir_levels)
@@ -246,44 +246,24 @@ contains
     CMP(msgtag_int_dipole_result)
     CMP(msgtag_response_setup)
     CMP(msgtag_response_level_send)
-    CMP(msgtag_response_chmat)
+    !MP(msgtag_response_chmat)
     CMP(msgtag_response_2index)
-    CMP(msgtag_resp_2in_sl_start)
+    !MP(msgtag_resp_2in_sl_start)
     CMP(msgtag_response_2in_send)
-    CMP(msgtag_response_2in_send2)
-    CMP(msgtag_response_3index)
-    CMP(msgtag_resp_sl_start_3ind)
-    CMP(msgtag_resp_3ind_fitdim)
-    CMP(msgtag_response_3ind_send)
+    !MP(msgtag_response_2in_send2)
+    !MP(msgtag_response_3index)
+    !MP(msgtag_resp_sl_start_3ind)
+    !MP(msgtag_resp_3ind_fitdim)
+    !MP(msgtag_response_3ind_send)
     CMP(msgtag_response_3Clb_start)
-    CMP(msgtag_resp_sl_start_3Clb)
+    !MP(msgtag_resp_sl_start_3Clb)
     CMP(msgtag_response_3Clb_send)
-    CMP(msgtag_resp_2center)
-    CMP(msgtag_send_all_levels)
-    CMP(msgtag_resp_3Clb_tmptape)
-    CMP(msgtag_resp_3Clb_tmptape_2)
-    CMP(msgtag_resp_3center_co)
-    CMP(msgtag_tddft_eps_eta)
-    CMP(msgtag_tddft_clshell)
-    CMP(msgtag_tddft_opshell)
-    CMP(msgtag_tddft_diagonl)
-    CMP(msgtag_tddft_diagnup)
-    CMP(msgtag_tddft_diagndn)
-    CMP(msgtag_tddft_parmlt2)
-    CMP(msgtag_tddft_parmlt3)
-    CMP(msgtag_tddft_parmlt4)
-    CMP(msgtag_tddft_dipolem)
-    CMP(msgtag_tddft_diagsnd)
-    CMP(msgtag_tddft_sendeig)
-    CMP(msgtag_tddft_dealloM)
-    CMP(msgtag_send_2c_Colmb)
-    CMP(msgtag_send_3c_Colmb)
-    CMP(msgtag_send_2c_start)
-    CMP(msgtag_tmp_3co_send)
-    CMP(msgtag_nori_2c_send)
-    CMP(msgtag_grid_send_n_points)
-    CMP(msgtag_grid_send_points)
-    CMP(msgtag_int_setup)
+    !MP(msgtag_send_all_levels)
+    !MP(msgtag_resp_3Clb_tmptape)
+    !MP(msgtag_resp_3Clb_tmptape_2)
+    !MP(msgtag_grid_send_n_points)
+    !MP(msgtag_grid_send_points)
+    !MP(msgtag_int_setup)
     CMP(msgtag_space_point)
     CMP(msgtag_finish_poten)
     CMP(msgtag_geom_grad)
@@ -292,8 +272,8 @@ contains
     CMP(msgtag_sp_grinfo_dealloc)
     CMP(msgtag_start_field)
     CMP(msgtag_finish_field)
-    CMP(msgtag_start_rho)
-    CMP(msgtag_finish_rho)
+    !MP(msgtag_start_rho)
+    !MP(msgtag_finish_rho)
     CMP(msgtag_free_bnds_fld)
     CMP(msgtag_epe_data_sent)
     CMP(msgtag_epe_do_gradients)
@@ -312,23 +292,51 @@ contains
     CMP(msgtag_epe_def_fin)
     CMP(msgtag_stop_daemon)
     CMP(msgtag_packed_message)
-    CMP(msgtag_response_rhs_h)
-    CMP(msgtag_resp_sl_start_rhs)
-    CMP(msgtag_resp_rhs_fitdim)
-    CMP(msgtag_response_rhs_send)
-    CMP(msgtag_resp_rho1dat)
-    CMP(msgtag_resp_rho1dat_1b)
-    CMP(msgtag_resp_all3index)
-    CMP(msgtag_resp_all3Clb_start)
-    CMP(msgtag_resp_addFcoul)
-    CMP(msgtag_response_FG)
-    CMP(msgtag_resp_FGstart)
-    CMP(msgtag_resp_FGslave)
-    CMP(msgtag_resp_FGmaster)
-    CMP(msgtag_resp_Fcoulstart)
-    CMP(msgtag_resp_Fcoulslave)
-    CMP(msgtag_resp_Fcoulmaster)
-    CMP(msgtag_end)
+    !MP(msgtag_response_rhs_h)
+    !MP(msgtag_resp_sl_start_rhs)
+    !MP(msgtag_resp_rhs_fitdim)
+    !MP(msgtag_response_rhs_send)
+    !MP(msgtag_resp_rho1dat)
+    !MP(msgtag_resp_rho1dat_1b)
+    !MP(msgtag_resp_all3index)
+    !MP(msgtag_resp_all3Clb_start)
+    !MP(msgtag_resp_addFcoul)
+    !MP(msgtag_response_FG)
+    !MP(msgtag_resp_FGstart)
+    !MP(msgtag_resp_FGslave)
+    !MP(msgtag_resp_FGmaster)
+    !MP(msgtag_resp_Fcoulstart)
+    !MP(msgtag_resp_Fcoulslave)
+    !MP(msgtag_resp_Fcoulmaster)
+    !MP(msgtag_cpks_xc)
+    CMP(msgtag_cpks_solv1)
+    CMP(msgtag_cpks_solv2)
+    CMP(msgtag_cpks_solv3)
+    CMP(msgtag_cpks_solv4)
+    CMP(msgtag_resp_2center)
+    !MP(msgtag_resp_3center_co)
+    CMP(msgtag_tddft_eps_eta)
+    CMP(msgtag_tddft_clshell)
+    !MP(msgtag_tddft_opshell)
+    CMP(msgtag_tddft_diagonl)
+    !MP(msgtag_tddft_diagnup)
+    !MP(msgtag_tddft_diagndn)
+    !MP(msgtag_tddft_parmlt2)
+    CMP(msgtag_tddft_parmlt3)
+    !MP(msgtag_tddft_parmlt4)
+    CMP(msgtag_tddft_dipolem)
+    !MP(msgtag_tddft_diagsnd)
+    CMP(msgtag_tddft_sendeig)
+    CMP(msgtag_tddft_dealloM)
+    CMP(msgtag_send_2c_Colmb)
+    !MP(msgtag_send_3c_Colmb)
+    CMP(msgtag_send_2c_start)
+    CMP(msgtag_tmp_3co_send)
+    CMP(msgtag_nori_2c_send)
+    CMP(msgtag_ind_dipmom)
+    CMP(msgtag_Pol_ham)
+    CMP(msgtag_back_Pol_ham)
+    !MP(msgtag_end)
     CMP(msgtag_error)
 
     if(name=='undef')then
